0ae8058721ff28933a595f2838af62d8afdd8f19
decoherer <63128649+decoherer@users.noreply.github.com>
2024-05-25 16:13:26 -0600

    Added methods expose,widths2wave

--- stackmap.py
+++ stackmap.py
@@ -7,7 +7,7 @@ class Stackmap():
             self.stack = [Wave(2*[y0],[x0,x1],'')]
             self.stackhistory = []
             self.titlehistory = []
-            self.colors = {'SiO₂':'#9dd0d4','Al₂O₃':'#a47fad'}
+            self.colors = {'SiO₂':'#9dd0d4','Al₂O₃':'#a47fad','positive resist':'#f288b6','negative resist':'#d26896'}
             self.colors.update(dict(resist='#f288b6',tfln='#d17a24',quartz='#d7dbda',
                                metal='#edcf4a',gold='#edcf4a',sio2='#9dd0d4',al2o3='#a47fad',
                                ti='#c9c2d1',au='#edcf4a',si='#f0b27a')) # si last since si is in sio2
@@ -16,23 +16,20 @@ class Stackmap():
             for k in self.colors:
                 if k.lower() in name.lower(): return self.colors[k]
             return None
-        def widths2wave(self,widths,invert=False):
+        def widths2wave(self,widths,thickness=1,invert=False):
             d = self.x1-self.x0-np.nansum(widths)
             n = np.sum(np.isnan(widths))
+            assert d>=0 and (n>0 or d==0), 'widths too large'
             widths = np.nan_to_num(widths,nan=d/n)
             wy = [0] + [0,1,1,0]*(len(widths)//2) + [0]
             wy = [1-y for y in wy] if invert else wy
             wx = [xx for x in np.cumsum([0]+list(widths)) for xx in [x,x]]
             wx = self.x0 + np.array(wx[1:-1])
-            return Wave(wy,wx)
-        
-
+            return thickness*Wave(wy,wx)
         def liftoff(self,liftoffdepth=2,title='Liftoff'):
             # assumes liftoff layer is the penulitmate layer
             assert 2==liftoffdepth, '2<liftoffdepth not yet implemented'
             assert liftoffdepth<len(self.stack)
-            self.stackhistory += [self.stack[:]]
-            self.titlehistory += [title]
             c,b,a = self.stack[-3:]
             b = b.mergex(a)
             c = c.mergex(b)
@@ -40,53 +37,49 @@ class Stackmap():
             anew = Wave(np.where(isthick,c.y,a.y),a.x,a.name)
             bnew = Wave(c.y,c.x,'',lw=0)
             self.stack = self.stack[:-2] + [bnew,anew]
+            self.stackhistory += [self.stack[:]]
+            self.titlehistory += [title]
             return self
         def strip(self,layerdepth=1,title='Strip'):
             # assumes strip layer is the penulitmate layer
             assert layerdepth+1<len(self.stack)
-            self.stackhistory += [self.stack[:]]
-            self.titlehistory += [title]
-            # b,a = self.stack[-2:]
-            # n = len(self.stack)-layerdepth
-            # c,b = self.stack[n-1:n+1]
-            # bnew = Wave(c.y,c.x,'',lw=0)
-            # self.stack = self.stack[:n] + [bnew] + self.stack[n+1:]
-
             n = len(self.stack)-layerdepth
             c,b,*aa = self.stack[n-1:]
             bnew = Wave(c.y,c.x,'',lw=0)
             delta = b.addlayer(-c)
             aanew = [a.addlayer(-delta).rename(a.name) for a in aa]
             self.stack = self.stack[:n] + [bnew] + aanew
-            return self
-            # todo: check 'warning: non-zero thickness above stripped layer'
-
-        def etch(self,w,title='Etch'):
-            assert np.all(0<=w)
             self.stackhistory += [self.stack[:]]
             self.titlehistory += [title]
+            return self
+            # todo: check 'warning: non-zero thickness above stripped layer'
+        def etch(self,w,widths=None,title='Etch',invert=False):
+            w = w if widths is None else self.widths2wave(widths,w,invert)
+            assert np.all(0<=w)
             profile = self.stack[-1].addlayer(-w)
             self.stack = [u.minimumlayer(profile) for u in self.stack]
+            self.stackhistory += [self.stack[:]]
+            self.titlehistory += [title]
             return self
-        def exposedevelop(self,w,mask,title0='Expose',title1='Develop',airgap=1,maskdepth=2):
+        def expose(self,w,widths=None,mask=None,title='Expose',airgap=1,maskdepth=1,invert=False):
+            w = w if widths is None else self.widths2wave(widths,w,invert)
             # mask = +1 for positive resist, -1 for negative resist
             u0 = self.stack[-1]
             assert (+1==mask and 'positive resist' in u0.name.lower()) or (-1==mask and 'negative resist' in u0.name.lower())
             uair = (airgap+max(u0)+0*u0).rename('').setplot(c='#ffffff00',lw=0)
             wmask = maskdepth*np.sign(w if -1==mask else max(w)-w)
-            umask = uair.addlayer(wmask).rename('mask').setplot(c='k',lw=0)
-            self.stack += [uair,umask]
-            self.plot()
-            self.stack = self.stack[:-2]
-            self.stackhistory += [self.stack]
-            self.titlehistory += [title1]
-
-            self.etch(w,title=title1)
-
+            umask = uair.addlayer(wmask).rename('photomask').setplot(c='#000000bb',lw=0)
+            self.stackhistory += [self.stack[:] + [uair,umask]]
+            self.titlehistory += [title]
             return self
-        
-        def develop(self,w,title='Develop',mask=0):
+        def develop(self,w,widths=None,title='Develop',invert=False):
+            w = w if widths is None else self.widths2wave(widths,w,invert)
             return self.etch(w,title=title)            
+        def exposeanddevelop(self,w,widths=None,mask=None,title0='Expose',title1='Develop',airgap=1,maskdepth=2,invert=False):
+            w = w if widths is None else self.widths2wave(widths,w,invert)
+            self.expose(w,mask=mask,title=title0,airgap=airgap,maskdepth=maskdepth)
+            self.develop(w,title=title1)
+            return self
         def masketch(self,depth,title='Etch'):
             # top layer assumed to be the mask for the etch
             assert 1<len(self.stack)
@@ -96,26 +89,27 @@ class Stackmap():
             return self.etch(w,title=title)
         def deposit(self,w,name='',title=''):
             title = title if title else f'Deposit {name}'
-            return self.addlayer(w,name,title=title)
-        def addlayer(self,w,name='',title=''):
+            return self.addlayer(w,name=name,title=title)
+        def addlayer(self,w,widths=None,name='',title='',invert=False):
+            w = w if widths is None else self.widths2wave(widths,w,invert)
             title = title if title else name
-            self.stackhistory += [self.stack[:]]
-            self.titlehistory += [f'{title}']
             w = w if isinstance(w,Wave) else Wave([w,w],[self.x0,self.x1])
             layer = self.stack[-1].addlayer(w)
             self.stack += [layer.rename(name)]
+            self.stackhistory += [self.stack[:]]
+            self.titlehistory += [f'{title}']
             return self
-        def addsymlayer(self,thickness,widths,name='',title='',invert=False):
-            assert 1==len(widths)%2 and np.all(np.array(widths)==np.array(widths[::-1]))
-            # print(self.widths2wave([np.nan]+widths+[np.nan],invert=invert))
-            d = 0.5*(self.x1-self.x0-np.sum(widths))
-            widths = [d]+list(widths)+[d]
-            wy = [0] + [0,1,1,0]*(len(widths)//2) + [0]
-            wy = [1-y for y in wy] if invert else wy
-            wx = [xx for x in np.cumsum([0]+list(widths)) for xx in [x,x]]
-            wx = np.array(wx[1:-1])-0.5*np.sum(widths)
-            self.addlayer(thickness*Wave(wy,wx),name,title=title)
-            return self
+        # def addsymlayer(self,thickness,widths,name='',title='',invert=False):
+        #     assert 1==len(widths)%2 and np.all(np.array(widths)==np.array(widths[::-1]))
+        #     # print(self.widths2wave([np.nan]+widths+[np.nan],invert=invert))
+        #     d = 0.5*(self.x1-self.x0-np.sum(widths))
+        #     widths = [d]+list(widths)+[d]
+        #     wy = [0] + [0,1,1,0]*(len(widths)//2) + [0]
+        #     wy = [1-y for y in wy] if invert else wy
+        #     wx = [xx for x in np.cumsum([0]+list(widths)) for xx in [x,x]]
+        #     wx = np.array(wx[1:-1])-0.5*np.sum(widths)
+        #     self.addlayer(thickness*Wave(wy,wx),name=name,title=title)
+        #     return self
         def ppt(self,title,**kwargs):
             save = f"figs/{kwargs.pop('save',title)}.pptx"
             from pypowerpoint import Presentation,addadvrslide
@@ -128,7 +122,8 @@ class Stackmap():
             prs.save(save)
             return prs
         def plothistory(self,title,**kwargs):
-            stacks = self.stackhistory[1:]+[self.stack]
+            # stacks = self.stackhistory[1:]+[self.stack]
+            stacks = self.stackhistory
             N = max([len(stack) for stack in stacks])
             cc = kwargs.pop('c','01234567890123456789'[:N])
             for n,stack in enumerate(stacks):
@@ -149,19 +144,13 @@ class Stackmap():
 
 if __name__ == '__main__':
     L = Stackmap(-20,20,-5,15)
-    L.addlayer(5,'quartz')
-    L.addlayer(3,'silicon 3µm')
-
-    # L.deposit(0.5,'positive resist 0.5µm')
-    # L.exposedevelop(0.5*Wave([0,0,1,1,0,0,1,1,0,0],[-20,-10,-10,-5,-5,5,5,10,10,20]),mask=+1)
-
-    L.addsymlayer(0.5,[5,10,5],'resist 0.5µm',invert=1)
+    L.addlayer(5,name='quartz')
+    L.addlayer(3,name='silicon 3µm')
+    L.deposit(0.5,'positive resist 0.5µm')
+    L.exposeanddevelop(0.5,[np.nan,5,10,5,np.nan],mask=+1)
     L.masketch(1.5)
-    L.addlayer(1,'metal 1µm')
+    L.deposit(1,'metal 1µm')
     L.liftoff()
-    L.addlayer(2,'SiO₂ 2µm')#.plot()
+    L.addlayer(2,name='SiO₂ 2µm')#.plot()
     L.ppt('stackmap example')
-
     L.plot()
-
-    # todo: wave from list of widths
